99737933dc41ad232196c0a7d0946b2540720e05
saurabhseth2495 <82148878+saurabhseth2495@users.noreply.github.com>
2021-04-27 12:50:50 +0530

    change in leap year

--- LeapYear.kt
+++ LeapYear.kt
@@ -1,15 +1,6 @@
 fun main(args: Array<String>) {
     print("please enter year :- ")
-    var leap: Boolean = false
     var year: Int = Integer.valueOf(readLine())
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                leap = true
-            }
-        } else {
-            leap = true
-        }
-    }
+    var leap = ((year % 400) == 0) || (((year % 4) == 0) && ((year % 100) != 0))
     println(if (leap) "$year is leap year" else "$year is not leap year")
 }
